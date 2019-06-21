ea4414a1e801270e304696dad0dfc2bc55e48147
kubichura <rizal.muhamad@ayomedia.com>
2019-06-21 14:23:32 +0700

    initial commit

--- resources/views/admin/post/add.blade.php
+++ resources/views/admin/post/add.blade.php
@@ -89,19 +89,6 @@
 <!-- FastClick -->
 <script src="{{ asset('lte/plugins/fastclick/fastclick.js') }}"></script>
 
-<!-- page script -->
-{{-- <script>
-    $(function () {
-          $("#example1").DataTable({
-      "paging": true,
-      "lengthChange": true,
-      "searching": true,
-      "ordering": true,
-      "info": true,
-      "autoWidth": true
-    });
-        });
-</script> --}}
 
 <script>
     $(document).ready(function(){
@@ -118,82 +105,30 @@
                                 };
                             };
 
-    });
-    $(function() {
-        $('#mytable').DataTable({
-            processing: true,
-            serverSide: true,
-            ajax: 'p/read-all',
-            columns: [
-                { data: 'id', name: 'id' },
-                { data: 'post_title', name: 'title' },
-                { data: 'post_summary', name: 'summary' },
-                { data: 'post_image_thumb', name: 'image' },
-                { data: 'id', name: 'action' }
-            ]
-        });
-    });
-</script>
+                            $('#mytable').DataTable({
+                                processing: true,
+                                serverSide: true,
+                                ajax: 'p/read-all',
+                                columns: [
+                                    { data: 'id', name: 'id' },
+                                    { data: 'post_title', name: 'title' },
+                                    { data: 'post_summary', name: 'summary' },
+                                    { data: 'post_image_thumb', name: 'image' },
+                                    { data: 'id', name: 'action' }
+                                ],
 
-<script type="text/javascript">
-    $(document).ready(function() {
-        $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
-        {
-            return {
-                "iStart": oSettings._iDisplayStart,
-                "iEnd": oSettings.fnDisplayEnd(),
-                "iLength": oSettings._iDisplayLength,
-                "iTotal": oSettings.fnRecordsTotal(),
-                "iFilteredTotal": oSettings.fnRecordsDisplay(),
-                "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
-                "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
-            };
-        };
+                                order: [[0, 'desc']],
+                                rowCallback: function(row, data, iDisplayIndex) {
+                                    var info = this.fnPagingInfo();
+                                    var page = info.iPage;
+                                    var length = info.iLength;
+                                    var index = page * length + (iDisplayIndex + 1);
+                                    $('td:eq(0)', row).html(index);
+                                }
+                            });
 
-        var t = $("#mytable").dataTable({
-            initComplete: function() {
-                var api = this.api();
-                $('#mytable_filter input')
-                        .off('.DT')
-                        .on('keyup.DT', function(e) {
-                            if (e.keyCode == 13) {
-                                api.search(this.value).draw();
-                    }
-                });
-            },
-            oLanguage: {
-                sProcessing: "loading..."
-            },
-            processing: true,
-            serverSide: true,
-            ajax: {"url": "{{ URL::to('p/read-all') }}", "type": "POST",
-success: function(data){
-console.log(data);
-}},
-// columns: [
-// {
-// "data": "id",
-// "orderable": false
-// },
-// {"data": "post_title"},
-// {"data": "post_summary"},
-// {"data": "post_image_thumb"},
-// {
-// "data" : "user_id",
-// "orderable": false,
-// "className" : "text-center"
-// }
-// ],
-order: [[0, 'desc']],
-rowCallback: function(row, data, iDisplayIndex) {
-var info = this.fnPagingInfo();
-var page = info.iPage;
-var length = info.iLength;
-var index = page * length + (iDisplayIndex + 1);
-$('td:eq(0)', row).html(index);
-}
-});
-});
+
+    });
 </script>
 
 <script>
